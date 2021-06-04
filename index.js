dc963cb2fba71e0e8346d5baeac052c2eea9ed13
GrzegorzNecka <strzypior@gmail.com>
2021-06-04 11:15:25 +0200

    1 file modified, 1 added, 1 deleted

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Import stylesheets
 import './style.css';
-import { setDraggable, setDropable } from './dragAndDropAttr';
+import { setDraggable, setDropable } from './setDragAndDropAttr';
 
 const dragElems = document.querySelectorAll('article');
 const dragArea = document.querySelectorAll('main')[0];
@@ -8,6 +8,11 @@ const dropArea = document.querySelectorAll('main')[1];
 
 setDraggable();
 setDropable();
+
+
+if(!"draggable" in document.createElement('span')) return;
+
+
 
 console.log(dragElems);
 console.log(dropArea);
