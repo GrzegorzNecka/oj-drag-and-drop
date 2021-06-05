9e5a9ba20076d87f5c755ef6f5e16199be786de6
GrzegorzNecka <strzypior@gmail.com>
2021-06-05 17:16:24 +0200

    1 file modified, 1 added

--- index.js
+++ index.js
@@ -9,19 +9,36 @@ const dropArea = document.querySelectorAll('main')[1];
 isDraggableSupported();
 
 draggablesElems.forEach(article => {
-  article.addEventListener('dragstart', () => {
-    article.classList.add('dragging');
+  article.addEventListener('dragstart', e => {
+    e.dataTransfer.effectAllowed = 'move';
+    e.dataTransfer.setData('text/html', article.outerHTML);
+    // article.classList.add('dragging');
   });
 
-  article.addEventListener('dragend', () => {
-    article.classList.remove('dragging');
+  article.addEventListener('dragend', e => {
+    // article.classList.remove('dragging');
+    if (e.dataTransfer.dropEffect == 'move') {
+      article.parentNode.removeChild(article);
+    }
   });
 });
 
 dropArea.addEventListener('dragover', e => {
   e.preventDefault();
+  return false;
+});
+
+dropArea.addEventListener('drop', e => {
+  e.preventDefault();
   console.log('drag over');
 
-  const draggable = document.querySelector('.dragging');
-  dropArea.appendChild(draggable);
+  // const draggable = document.querySelector('.dragging');
+  // dropArea.appendChild(draggable);
+  const data = e.dataTransfer.getData('text/html');
+  const fragment = document.createRange().createContextualFragment(data);
+
+  // dropArea.appendChild(JSON.parse(data));
+  // console.log(JSON.parse(data));
+  console.log(fragment);
+  dropArea.appendChild(fragment);
 });
