7e5edba0599ad063b1ef24d237582749ce5acfdf
GrzegorzNecka <strzypior@gmail.com>
2021-06-05 16:56:11 +0200

    3 files modified

--- index.js
+++ index.js
@@ -4,15 +4,24 @@ import { setDraggable, setDropable } from './setDragAndDropAttr';
 import { isDraggableSupported } from './isDraggableSupported';
 
 const draggablesElems = document.querySelectorAll('article');
-const dragArea = document.querySelectorAll('main')[0];
 const dropArea = document.querySelectorAll('main')[1];
-isDraggableSupported();
-// setDraggable();
-// setDropable();
 
-// console.log(dragElems);
-// console.log(dropArea);
+isDraggableSupported();
 
 draggablesElems.forEach(article => {
-  article.addEventListener('dragstart', () => {});
+  article.addEventListener('dragstart', () => {
+    article.classList.add('dragging');
+  });
+
+  article.addEventListener('dragend', () => {
+    article.classList.remove('dragging');
+  });
+});
+
+dropArea.addEventListener('dragover', e => {
+  e.preventDefault();
+  console.log('drag over');
+
+  const draggable = document.querySelector('.dragging');
+  dropArea.appendChild(draggable);
 });
